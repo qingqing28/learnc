61c7a19bda18a76da34336a6d6334751ceaffea1
PQQ <qqpengyc@163.com>
2020-11-17 16:53:37 +0800

    打印1000-2000年之间的闰年

--- learnC/learnC/leap_year_1117.c
+++ learnC/learnC/leap_year_1117.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
+int main1() {
 	//打印1000年到2000年之间的闰年
 	int year;
 	for (year = 1000; year <= 2000; year++) {
@@ -12,16 +12,16 @@ int main() {
 				printf("%d是闰年", year);
 				printf("\n");
 			}
-		}else {
+		}
+		else {
 			//普通闰年
 			if (year % 4 == 0) {
 				printf("%d是闰年", year);
 				printf("\n");
 			}
 		}
-		
 	}
-	
 	system("pause");
 	return 0;
+
 }
